3fe36b43bc06c3f50869c434d6258dc9fd9b92f4
Richi <richard.hartung@smail.inf.h-brs.de>
2024-09-21 10:41:38 +0200

    add: backend retrieve persons

--- backend/artifact/src/main/java/group/artifact/controller/PersonController.java
+++ backend/artifact/src/main/java/group/artifact/controller/PersonController.java
@@ -1,8 +1,10 @@
 package group.artifact.controller;
 
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -23,6 +25,17 @@ public class PersonController {
             personService.save(p);
             return ResponseEntity.ok(
                     "person successfully saved");
+        } catch (Exception e) {
+            System.out.println("ERROR: " + e);
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(null);
+        }
+    }
+
+    @GetMapping("/receive/persons")
+    public ResponseEntity<List<Person>> receivePersons() {
+        try {
+            List<Person> persons = personService.getAllPersons();
+            return ResponseEntity.ok(persons);
         } catch (Exception e) {
             System.out.println("ERROR: " + e);
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(null);
--- backend/artifact/src/main/java/group/artifact/service/PersonService.java
+++ backend/artifact/src/main/java/group/artifact/service/PersonService.java
@@ -1,5 +1,7 @@
 package group.artifact.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import group.artifact.repository.PersonRepository;
@@ -21,6 +23,15 @@ public class PersonService {
             // Log the exception
             System.out.println("Error occurred while saving person: " + e.getMessage());
             throw new RuntimeException("Failed to save person", e);
+        }
+    }
+
+    public List<Person> getAllPersons() {
+        try {
+            return personRepository.findAll();
+        } catch (Exception e) {
+            System.out.println("Error occurred while retrieving persons: " + e.getMessage());
+            throw new RuntimeException("Failed to retrieve persons", e);
         }
     }
 }
