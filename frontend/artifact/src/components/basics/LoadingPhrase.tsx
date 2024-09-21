02f8a61a99aed6b83b7579b390870237e3a33263
Richi <richard.hartung@smail.inf.h-brs.de>
2024-09-21 20:07:33 +0200

    removed: big loading dots

--- frontend/artifact/src/components/basics/LoadingPhrase.tsx
+++ frontend/artifact/src/components/basics/LoadingPhrase.tsx
@@ -37,15 +37,6 @@ const LoadingAnimation = () => {
       <div className="text-lg font-semibold text-[#c3002d] mb-2">
         {currentPhrase}{dots}
       </div>
-      <div className="flex space-x-2">
-        {[...Array(3)].map((_, i) => (
-          <div
-            key={i}
-            className="w-3 h-3 bg-[#c3002d] rounded-full animate-bounce"
-            style={{ animationDelay: `${i * 0.2}s` }}
-          />
-        ))}
-      </div>
     </div>
   );
 };
